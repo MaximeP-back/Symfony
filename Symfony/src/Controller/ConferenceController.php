c31c1a770ba2a2a9283cbe9834d6e63e0b371dce
MaximeP-back <maxime.pizzuto@tiime.fr>
2024-10-07 11:58:43 +0200

    ajout index Controller

---
+++ Symfony/src/Controller/ConferenceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function index(Request $request): Response
+    {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('Hello %s!', htmlspecialchars($name));
+        }
+
+        return $this->render('conference/index.html.twig', [
+            'controller_name' => 'ConferenceController',
+            'greet' => $greet,
+        ]);
+    }
+}
